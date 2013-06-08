c298db8004c6c6219d7da6db09d5612032c102be
ilamaiolo <ilariore88@gmail.com>
2013-06-08 19:09:07 +0200

    edit exercise6

--- 2013-06-07/javascript/exercise6.js
+++ 2013-06-07/javascript/exercise6.js
@@ -3,34 +3,18 @@
 //characteristic matrix of 2D faces, in the file format OBJ.
 
 
-FV = [[6,7,8,9],
-[0,5,8],
-[0,4,5],
-[1,2,4,5],
-[2,4,5,1],
-[0,8,7], [3,6,7], [1,2,3], [0,1,4]
-]
-
-V = [[0,6],
-[0,0,1],
-[3,0],
-[6,0],
-[0,3],
-[3,3],
-[6,3,2],
-[6,6],
-[3,6]]
+FV = [[6,7,8,9],[0,5,8],[0,4,5],[1,2,4,5],[2,4,5,1],[0,8,7], [3,6,7], [1,2,3], [0,1,4]]
+V = [[0,6],[0,0,1],[3,0],[6,0],[0,3],[3,3],[6,3,2],[6,6],[3,6]]
 
 modelloLAR = [V,FV];
 
-function lar2Obj (model){
+function changeFormatFile (model){
 	var vertici = model[0];
 	var facceVertici = model[1];
 	var output = "";
 
 	for (var i = 0; i < vertici.length; i++){
-			output+= "vertici: ";
-			
+			output+= "vertici: ";		
 			if(vertici[i][2] !== undefined){
 				output+= vertici[i][0]+",  "+vertici[i][1]+",  "+vertici[i][2];
 			}
@@ -55,4 +39,4 @@ function lar2Obj (model){
 
 }
 
-lar2Obj(modelloLAR);
+changeFormatFile(modelloLAR);
